9d1d6e94c7bc3fd3f192e93caebf1fad1b234a43
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 15:33:18 -0500

    Update db_assignment.js

--- db_assignment.js
+++ db_assignment.js
@@ -26,16 +26,16 @@ app.get('/', function (req, res, next) {
     }
     query_result = []
     for (i = 0; i < rows.length; i++) {
+
       if (rows[i].reps == -1) {
-        rows[i].reps == ""
+        var reps == ""
+      } else {
+        var reps = rows[i].reps
       }
-      if (rows[i].weight == -1) {
-        rows[i].weight == ""
-      }
-      if (getFormattedDateYMR(rows[i].date) == "9999-12-31") {
-        row[i].date = ""
-      }
-      query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': rows[i].reps, 'weight': rows[i].weight, 'date': getFormattedDate(rows[i].date), 'unit': rows[i].unit })
+
+
+
+      query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': reps, 'weight': rows[i].weight, 'date': getFormattedDate(rows[i].date), 'unit': rows[i].unit })
     }
     context.results = query_result;
     res.render("home", context)
