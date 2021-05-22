f47b40bd0af0b805f09188ad9793dd040a4ca818
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 00:14:59 -0500

    Update

--- db_assignment.js
+++ db_assignment.js
@@ -68,6 +68,18 @@ app.post('/pull', function (req, res, next) {
 
 
 
+app.get('/insert', function (req, res, next) {
+  var context = {};
+  mysql.pool.query("INSERT INTO exercise (`name`,`reps`,`weight`,`date`,`unit`) VALUES (?,?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.unit], function (err, result) {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.redirect("/pull")
+
+  });
+});
+
 
 
 
