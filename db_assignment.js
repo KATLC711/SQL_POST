06cb0d944407bf2075daed0862f901e752bc94e4
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 00:52:03 -0500

    Update db_assignment.js

--- db_assignment.js
+++ db_assignment.js
@@ -88,72 +88,62 @@ app.get('/insert', function (req, res, next) {
 
 
 
-app.post('/insert', function (req, res, next) {
-  console.log("POST INSERT")
-  mysql.pool.query("INSERT INTO exercise (`name`,`reps`,`weight`,`date`,`unit`) VALUES (?,?,?,?,?)", [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.unit], function (err, result) {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.redirect("/pull_post")
-  });
+
+
+
+
+app.use(function (req, res) {
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
 
 
 
 
 
-  app.use(function (req, res) {
-    res.status(404);
-    res.render('404');
-  });
+function getFormattedDate(date_unformmated) {
 
-  app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-  });
+  var date = new Date(date_unformmated)
 
-  app.listen(app.get('port'), function () {
-    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-  });
+  var year = date.getFullYear();
 
+  var month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : '0' + month;
 
+  var day = date.getDate().toString();
+  day = day.length > 1 ? day : '0' + day;
 
+  final = month + '-' + day + '-' + year;
 
-
-  function getFormattedDate(date_unformmated) {
-
-    var date = new Date(date_unformmated)
-
-    var year = date.getFullYear();
-
-    var month = (1 + date.getMonth()).toString();
-    month = month.length > 1 ? month : '0' + month;
-
-    var day = date.getDate().toString();
-    day = day.length > 1 ? day : '0' + day;
-
-    final = month + '-' + day + '-' + year;
-
-    return final.toString();
-  }
+  return final.toString();
+}
 
 
 
 
-  function getFormattedDateYMR(date_unformmated) {
+function getFormattedDateYMR(date_unformmated) {
 
-    var date = new Date(date_unformmated)
+  var date = new Date(date_unformmated)
 
-    var year = date.getFullYear();
+  var year = date.getFullYear();
 
-    var month = (1 + date.getMonth()).toString();
-    month = month.length > 1 ? month : '0' + month;
+  var month = (1 + date.getMonth()).toString();
+  month = month.length > 1 ? month : '0' + month;
 
-    var day = date.getDate().toString();
-    day = day.length > 1 ? day : '0' + day;
+  var day = date.getDate().toString();
+  day = day.length > 1 ? day : '0' + day;
 
-    final = year + '-' + month + '-' + day;
+  final = year + '-' + month + '-' + day;
 
-    return final.toString();
-  }
+  return final.toString();
+}
