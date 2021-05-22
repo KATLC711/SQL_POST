39d9474af9118533ebcb77a67c95155c4901cdde
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 01:44:43 -0500

    Update

--- db_assignment.js
+++ db_assignment.js
@@ -183,6 +183,35 @@ app.get('/edit_get', function (req, res, next) {
 
 
 
+app.post('/edit_post', function (req, res, next) {
+  var context = {};
+  mysql.pool.query("SELECT * FROM exercise WHERE id=?", [req.query.id], function (err, result) {
+    if (err) {
+      next(err);
+      return;
+    }
+    if (result.length == 1) {
+      var curVals = result[0];
+      mysql.pool.query("UPDATE exercise SET name=?,reps=?,weight=?,date=?,unit=? WHERE id=? ",
+        [req.body.name || curVals.name, req.body.reps || curVals.reps, req.body.weight || curVals.weight, req.body.date || curVals.date, req.body.unit || curVals.unit, req.body.id],
+        function (err, result) {
+          if (err) {
+            next(err);
+            return;
+          }
+          query_result = []
+          for (i = 0; i < rows.length; i++) {
+            query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': rows[i].reps, 'weight': rows[i].weight, 'date': getFormattedDate(rows[i].date), 'unit': rows[i].unit })
+          }
+          context.results = JSON.stringify(rows);
+          res.send(context);
+        });
+    }
+  });
+});
+
+
+
 
 
 
