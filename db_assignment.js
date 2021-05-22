8a08d6609d8e366efee807046b14e8501adc674b
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 02:00:22 -0500

    Update db_assignment.js

--- db_assignment.js
+++ db_assignment.js
@@ -201,12 +201,7 @@ app.post('/edit_post', function (req, res, next) {
             next(err);
             return;
           }
-          query_result = []
-          for (i = 0; i < rows.length; i++) {
-            query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': rows[i].reps, 'weight': rows[i].weight, 'date': getFormattedDate(rows[i].date), 'unit': rows[i].unit })
-          }
-          context.results = JSON.stringify(rows);
-          res.send(context);
+          res.redirect("/pull_get");
         });
     }
   });
