2971b77a733ceabc81551554c21381c776a38cc4
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 00:48:37 -0500

    Update db_assignment.js

--- db_assignment.js
+++ db_assignment.js
@@ -3,7 +3,11 @@ var mysql = require('./dbcon.js');
 
 var app = express();
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.use(express.static('public'));
 app.engine('handlebars', handlebars.engine);
