eb9c9f3d9db970f2a8cd49c036143def69864e01
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 14:38:10 -0500

    Update db_assignment.js

--- db_assignment.js
+++ db_assignment.js
@@ -60,6 +60,7 @@ app.post('/', function (req, res, next) {
           query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': rows[i].reps, 'weight': rows[i].weight, 'date': getFormattedDate(rows[i].date), 'unit': rows[i].unit })
         }
         context.results = JSON.stringify(query_result);
+        console.log(context)
         res.send(context);
       });
 
