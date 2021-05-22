6802e14c5014feb88404c604dcf809bb98644568
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 14:36:55 -0500

    Update db_assignment.js

--- db_assignment.js
+++ db_assignment.js
@@ -48,6 +48,23 @@ app.post('/', function (req, res, next) {
         next(err);
         return;
       }
+
+
+      var context = {};
+      mysql.pool.query('SELECT * FROM exercise', function (err, rows, fields) {
+        if (err) {
+          next(err);
+          return;
+        }
+        //console.log(rows)
+        query_result = []
+        for (i = 0; i < rows.length; i++) {
+          query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': rows[i].reps, 'weight': rows[i].weight, 'date': getFormattedDate(rows[i].date), 'unit': rows[i].unit })
+        }
+        context.results = JSON.stringify(rows);
+        res.send(context);
+      });
+
     });
   } else if (req.body.typ == "Delete") {
     console.log("Delete")
@@ -61,20 +78,7 @@ app.post('/', function (req, res, next) {
 
 
 
-  var context = {};
-  mysql.pool.query('SELECT * FROM exercise', function (err, rows, fields) {
-    if (err) {
-      next(err);
-      return;
-    }
-    //console.log(rows)
-    query_result = []
-    for (i = 0; i < rows.length; i++) {
-      query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': rows[i].reps, 'weight': rows[i].weight, 'date': getFormattedDate(rows[i].date), 'unit': rows[i].unit })
-    }
-    context.results = JSON.stringify(rows);
-    res.send(context);
-  });
+
 });
 
 
