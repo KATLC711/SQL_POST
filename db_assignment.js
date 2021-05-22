66744952cc9500506321d39210ebc1aefa79186b
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 15:37:44 -0500

    Update db_assignment.js

--- db_assignment.js
+++ db_assignment.js
@@ -46,9 +46,15 @@ app.get('/', function (req, res, next) {
         var unit = rows[i].unit
       }
 
+      if (getFormattedDate(rows[i].date) == '12-31-9999') {
+        var date = ""
+      } else {
+        var date = rows[i].getFormattedDate(rows[i].date)
+      }
 
 
-      query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': reps, 'weight': weight, 'date': "", 'unit': unit })
+
+      query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': reps, 'weight': weight, 'date': date, 'unit': unit })
     }
     context.results = query_result;
     res.render("home", context)
