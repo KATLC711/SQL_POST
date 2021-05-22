eaa7fe835426bee0e320104beb392612289f0d4d
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-22 14:40:46 -0500

    Update db_assignment.js

--- db_assignment.js
+++ db_assignment.js
@@ -45,7 +45,6 @@ app.post('/', function (req, res, next) {
     mysql.pool.query("INSERT INTO exercise (`name`,`reps`,`weight`,`date`,`unit`) VALUES (?,?,?,?,?)", [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.unit], function (err, result) {
       if (err) {
         next(err);
-        console.log("Error")
         return;
       }
 
@@ -63,9 +62,23 @@ app.post('/', function (req, res, next) {
   }
 
 
-  console.log("Here")
 
 
+  var context = {};
+  mysql.pool.query('SELECT * FROM exercise', function (err, rows, fields) {
+    if (err) {
+      next(err);
+      return;
+    }
+    query_result = []
+    for (i = 0; i < rows.length; i++) {
+      query_result.push({ 'id': rows[i].id, 'name': rows[i].name, 'reps': rows[i].reps, 'weight': rows[i].weight, 'date': getFormattedDate(rows[i].date), 'unit': rows[i].unit })
+    }
+    context.results = JSON.stringify(query_result);
+    console.log(context)
+    res.send(context);
+
+  });
 });
 
 
